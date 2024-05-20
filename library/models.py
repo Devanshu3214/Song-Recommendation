a455dd44b7469764c1b2a15c539bebf0e99f0583
Devanshu <devyadav3214@gmail.com>
2024-05-20 20:06:29 +0530

    Sec commit

---
+++ library/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Data(models.Model): # Corrected the class name to start with uppercase and inherit from models.Model
+    song = models.CharField(max_length=50, blank=True)
+    year = models.IntegerField(default=0000, blank=True)  # Corrected the default value to an integer
+    artist = models.CharField(max_length=40, blank=True)
+    genre = models.CharField(max_length=40, blank=True)
+    rating = models.IntegerField(default=0)  # Corrected the default value to an integer
+    created_by = models.CharField(max_length=40, blank=True)
+    last_updated = models.DateTimeField(default=timezone.now)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)  # Add this line to associate with a user
+    export_to_csv = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.song
